9c1cf71ddac793d32f0b0e3d66994f5169686adb
Danny Avery <danny.w.avery@gmail.com>
2017-12-21 13:48:05 -0700

    Create chat rooms using AngularFire methods & UI Bootstrap $uibModal

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,25 @@
 (function() {
-  function HomeCtrl(Room) {
+  function HomeCtrl(Room, $uibModal) {
+    /**
+    * @desc Stores Room service
+    * @type {Object}
+    */
     this.roomList = Room;
+
+    /**
+    * @func open
+    * @desc Defines & opens modal
+    */
+    this.open = function() {
+      $uibModal.open({
+        templateUrl: '/templates/modal.html',
+        controller: 'ModalCtrl',
+        controllerAs: 'modal',
+      }).result.catch(function(){}, function(res){});
+    };
   }
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['Room', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
 })();
--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -4,7 +4,20 @@
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
 
+    /**
+    * @desc To store firebase array
+    * @type {Array}
+    */
     Room.all = rooms;
+
+    /**
+    * @func add
+    * @desc Adds room to rooms(firebase array)
+    */
+    Room.add = function(room) {
+      rooms.$add({ room });
+      this.room = '';
+    }
 
     return Room;
   }
